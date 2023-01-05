f5deb00976d3c91aa87690543378fd6c2ca73571
Brayan <brayanzuritadev@gmail.com>
2023-01-04 22:54:49 -0400

    first commit

---
+++ TimeZone.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using TimeZone;
+namespace TimeZone.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //preparacion
+            var classe = new TimeZone();
+            //ejecucion
+           // classe.Timezone();
+            //verificacion
+        }
+    }
+}
